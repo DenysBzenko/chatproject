48ed6972b247a38f2ce0dd16e1dd1cca70a6dfd5
Denys Bzenko <dbzenko@kse.org.ua>
2024-02-28 01:57:17 +0200

    ADD FULL CRUD

--- medic/medicchat/urls.py
+++ medic/medicchat/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import register_doctor, view_doctors , doctor_profile
+from .views import register_doctor, view_doctors , doctor_profile, chat_list, edit_doctor,delete_doctor
+
 
 urlpatterns = [
     path('', register_doctor, name='register_doctor'),
     path('doctors/', view_doctors, name='view_doctors'),
     path('profile/', doctor_profile, name='profile'),
+    path('chat/', chat_list, name='chat_list'),
+    path('edit_doctor/<int:doctor_id>/', edit_doctor, name='edit_doctor'),
+    path('delete_doctor/<int:doctor_id>/', delete_doctor, name='delete_doctor'),
 ]
--- medic/medicchat/views.py
+++ medic/medicchat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Doctor
+from .models import Doctor ,ChatRoom
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 def register_doctor(request):
     if request.method == 'POST':
@@ -20,3 +22,29 @@ def doctor_profile(request):
     # Отримання останнього зареєстрованого лікаря
     latest_doctor = Doctor.objects.latest('id')
     return render(request, 'profile.html', {'doctor': latest_doctor})
+def chat_list(request):
+    chats = ChatRoom.objects.all()
+    return render(request, 'chat_list.html', {'chats': chats})
+def chat_room(request, chat_id):
+    chat = ChatRoom.objects.get(id=chat_id)
+    return render(request, 'chat_room.html', {'chat': chat})
+
+def edit_doctor(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    if request.method == 'POST':
+        doctor.name = request.POST.get('name')
+        doctor.age = request.POST.get('age')
+        doctor.specialty = request.POST.get('specialty')
+        doctor.save()
+        return redirect('profile')
+    else:
+        return render(request, 'edit_profile.html', {'doctor': doctor})
+
+def delete_doctor(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    if request.method == 'POST':
+        if 'delete' in request.POST:
+            doctor.delete()
+            return redirect(reverse('register_doctor'))
+    return render(request, 'delete_profile.html', {'doctor': doctor})
+
