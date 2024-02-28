f38557b5dfc9e5e49cc3f1dd54119441d7953db5
Denys Bzenko <dbzenko@kse.org.ua>
2024-02-28 15:59:29 +0200

    Static

--- medic/medicchat/urls.py
+++ medic/medicchat/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import register_doctor, view_doctors, doctor_profile, chat_list, edit_doctor, delete_doctor
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', register_doctor, name='register_doctor'),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('delete_doctor/<int:doctor_id>/', delete_doctor, name='delete_doctor'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
